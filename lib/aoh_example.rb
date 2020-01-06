1c39ec767c079ef5861fec25118c1db8c5c29f7f
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-06 02:27:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,11 +20,11 @@ end
 
 def literal_aoh
   contacts = [
-    {:name  "Don Gately", occupation: "Live-in Staff"}
-    {:name  "Joelle van Dyne", occupation: "Radio Personality"}
-    {:name  "Pat Monteseian", occupation: "Staff"}
-    {:name  "Kate Gompert", occupation: "None"}
-    {:name  "Bruce Green", occupation: "Fan of Mildred"}
+    {:name =>  "Don Gately", :occupation => "Live-in Staff"}
+    {:name =>  "Joelle van Dyne", :occupation => "Radio Personality"}
+    {:name =>  "Pat Monteseian", :occupation => "Staff"}
+    {:name =>  "Kate Gompert", :occupation => "None"}
+    {:name =>  "Bruce Green", :occupation => "Fan of Mildred"}
     ]
     
     contacts

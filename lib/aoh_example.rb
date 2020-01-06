8bf42c37273fd0391c1d38a821212f096c090bfb
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-06 02:24:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,11 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   contacts = [
-    {name:  "Don Gately", occupation: "Live-in Staff"},
-    {name:  "Joelle van Dyne", occupation: "Radio Personality"},
-    {name:  "Pat Monteseian", occupation: "Staff"},
-    {name:  "Kate Gompert", occupation: "None"},
-    {name:  "Bruce Green", occupation: "Fan of Mildred"},
+    {name:  "Don Gately", occupation: "Live-in Staff"}
+    {name:  "Joelle van Dyne", occupation: "Radio Personality"}
+    {name:  "Pat Monteseian", occupation: "Staff"}
+    {name:  "Kate Gompert", occupation: "None" }
+    {name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
     
     contacts
